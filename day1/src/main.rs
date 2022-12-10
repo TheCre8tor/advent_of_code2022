7b1783a54151a94b86e13030e9b51701800d135a
TheCre8tor <cre8tor.alexander@gmail.com>
2022-12-10 20:39:59 +0100

    Completed the declarative type

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+mod iterator_version;
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    // On windows operating systems "\n" is "\r\n"
+    let file = include_str!("input.txt");
+    let lines = file
+        .lines()
+        .map(|v| v.parse::<u64>().ok())
+        .collect::<Vec<_>>();
+
+    let elven_lead = lines
+        .split(|line| line.is_none())
+        .map(|group| group.iter().map(|v| v.unwrap()).sum::<u64>())
+        .max();
+
+    println!("{elven_lead:?}");
+
+    iterator_version::run();
+
+    Ok(())
 }
